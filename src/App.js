f61e4f8647bf7f1608038912ac65491e35a63ddf
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-03-04 18:16:49 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import { useState } from "react";
+
+
 function App() {
+
+	const [count, setCount] = useState(0);
+
 	const handleClick = () => {
-		console.log("I've been clicked")
+		setCount(count + 1);
 
 
 	};
 
-
-
-
 	return (
 		<div>
 			<button onClick={handleClick}>Add Animal</button>
+			<div>
+				Number of animals: {count}
+			</div>
 		</div>
 	);
 
